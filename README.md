bb81a540b49ad4373f6f54fb05e1a5aa0ba18181
Will Xyen <willxinc@hotmail.com>
2014-08-28 09:12:52 -0400

    Base Import

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ odoo-payment_savedcc
 ====================
 
 Odoo Payment Module: Saved CC
+
+Basic implemntation of Saved CC payment method for use with website_sale
---
+++ payment_savedcc/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Saved CC Payment Acquirer',
+    'category': 'Hidden',
+    'summary': 'Payment Acquirer: Saved CC Implementation',
+    'version': '1.0',
+    'description': """Saved CC Payment Acquirer""",
+    'author': 'xyenDev',
+    'depends': ['payment'],
+    'data': [
+        'views/savedcc.xml',
+        'views/payment_acquirer.xml',
+        'data/savedcc.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
